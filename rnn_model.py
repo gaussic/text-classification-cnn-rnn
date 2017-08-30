cd3b3ebaeb403e7aef27128811e0313c40cbfe76
gaussic <dzkangshu@hotmail.com>
2017-08-30 17:16:50 +0800

    rnn model

--- rnn_model.py
+++ rnn_model.py
@@ -53,13 +53,16 @@ class TextRNN(object):
         """rnn模型"""
 
         def lstm_cell():
+            """lstm核"""
             return tf.contrib.rnn.BasicLSTMCell(self.config.hidden_dim,
                 state_is_tuple=True)
 
         def gru_cell():
+            """gru核"""
             return tf.contrib.rnn.GRUCell(self.config.hidden_dim)
 
         def dropout():
+            """为每一个rnn核后面加一个dropout层"""
             if (self.config.rnn == 'lstm'):
                 cell = lstm_cell()
             else:
@@ -71,12 +74,13 @@ class TextRNN(object):
         embedding_inputs = self.input_embedding()
 
         with tf.name_scope("rnn"):
+            # 多层rnn网络
             cells = [dropout() for _ in range(self.config.num_layers)]
             rnn_cell = tf.contrib.rnn.MultiRNNCell(cells, state_is_tuple=True)
 
             _outputs, _ = tf.nn.dynamic_rnn(cell=rnn_cell,
                 inputs=embedding_inputs, dtype=tf.float32)
-            last = _outputs[:, -1, :]
+            last = _outputs[:, -1, :]  # 取最后一个时序输出作为结果
 
         with tf.name_scope("score"):
             # 全连接层，后面接dropout以及relu激活
@@ -107,105 +111,3 @@ class TextRNN(object):
             correct_pred = tf.equal(tf.argmax(self.input_y, 1),
                 tf.argmax(self.pred_y, 1))
             self.acc = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-
-def run_epoch():
-
-    # 载入数据
-    print('Loading data...')
-    start_time = time.time()
-    config = TRNNConfig()
-    x_train, y_train, x_test, y_test, \
-        x_val, y_val, words = preocess_file()
-    config.vocab_size = len(words)
-
-    end_time = time.time()
-    time_dif = end_time - start_time
-    time_dif = timedelta(seconds=int(round(time_dif)))
-    print('Time usage:', time_dif)
-
-    print('Constructing Model...')
-    tcnn = TextRNN(config)
-    session = tf.Session()
-    session.run(tf.global_variables_initializer())
-
-    # 配置 tensorboard
-    tf.summary.scalar("loss", tcnn.loss)
-    tf.summary.scalar("accuracy", tcnn.acc)
-
-    tensorboard_dir = 'tensorboard/textcnn'
-    if os.path.exists(tensorboard_dir):
-        shutil.rmtree(tensorboard_dir)
-        os.makedirs(tensorboard_dir)
-
-    merged_summary = tf.summary.merge_all()
-    writer = tf.summary.FileWriter(tensorboard_dir)
-    writer.add_graph(session.graph)
-
-    # 生成批次数据
-    batch_train = batch_iter(list(zip(x_train, y_train)),
-        config.batch_size, config.num_epochs)
-
-    def feed_data(batch):
-        x_batch, y_batch = zip(*batch)
-        feed_dict = {
-            tcnn.input_x: x_batch,
-            tcnn.input_y: y_batch
-        }
-        return feed_dict, len(x_batch)
-
-    def evaluate(x_, y_):
-        """
-        模型评估
-        一次运行所有的数据会OOM，所以需要分批和汇总
-        """
-        batch_eval = batch_iter(list(zip(x_, y_)), 128, 1)
-
-        total_loss = 0.0
-        total_acc = 0.0
-        cnt = 0
-        for batch in batch_eval:
-            feed_dict, cur_batch_len = feed_data(batch)
-            loss, acc = session.run([tcnn.loss, tcnn.acc],
-                feed_dict=feed_dict)
-            total_loss += loss * cur_batch_len
-            total_acc += acc * cur_batch_len
-            cnt += cur_batch_len
-
-        return total_loss / cnt, total_acc / cnt
-
-    # 训练与验证
-    print('Training and evaluating...')
-    start_time = time.time()
-    for i, batch in enumerate(batch_train):
-        feed_dict, _ = feed_data(batch)
-
-        if i % 5 == 0:  # 每5次将训练结果写入tensorboard scalar
-            s = session.run(merged_summary, feed_dict=feed_dict)
-            writer.add_summary(s, i)
-
-        if i % 200 == 0:  # 每200次输出在训练集和验证集上的性能
-            loss_train, acc_train = session.run([tcnn.loss, tcnn.acc],
-                feed_dict=feed_dict)
-            loss, acc = evaluate(x_val, y_val)
-
-            # 时间
-            end_time = time.time()
-            time_dif = end_time - start_time
-            time_dif = timedelta(seconds=int(round(time_dif)))
-
-            msg = 'Iter: {0:>6}, Train Loss: {1:>6.2}, Train Acc: {2:>7.2%},'\
-                + ' Val Loss: {3:>6.2}, Val Acc: {4:>7.2%}, Time: {5}'
-            print(msg.format(i + 1, loss_train, acc_train, loss, acc, time_dif))
-
-        session.run(tcnn.optim, feed_dict=feed_dict)  # 运行优化
-
-    # 最后在测试集上进行评估
-    print('Evaluating on test set...')
-    loss_test, acc_test = evaluate(x_test, y_test)
-    msg = 'Test Loss: {0:>6.2}, Test Acc: {1:>7.2%}'
-    print(msg.format(loss_test, acc_test))
-
-    session.close()
-
-if __name__ == '__main__':
-    run_epoch()
