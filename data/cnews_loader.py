bd38986c469bea2bad7482d197386d1a1fc4b0d9
howie6879 <xiaozizayang@gmail.com>
2018-01-15 16:15:42 +0800

    Supported: python2.7

--- data/cnews_loader.py
+++ data/cnews_loader.py
@@ -1,17 +1,46 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import os
+import sys
 
 from collections import Counter
-import tensorflow.contrib.keras as kr
+
 import numpy as np
-import os
+import tensorflow.contrib.keras as kr
+
+if sys.version_info[0] > 2:
+    is_py3 = True
+else:
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+    is_py3 = False
+
+
+def native_word(word, encoding='utf-8'):
+    """如果在oython2下面使用python3训练的模型，可考虑调用此函数转化一下字符编码"""
+    if not is_py3:
+        return word.encode(encoding)
+    else:
+        return word
+
+
+def native_content(content):
+    if not is_py3:
+        return unicode(content)
+    else:
+        return content
+
 
 def open_file(filename, mode='r'):
     """
     Commonly used file reader, change this to switch between python2 and python3.
     mode: 'r' or 'w' for read or write
     """
-    return open(filename, mode, encoding='utf-8', errors='ignore')
+    if is_py3:
+        return open(filename, mode, encoding='utf-8', errors='ignore')
+    else:
+        return open(filename, mode)
+
 
 def read_file(filename):
     """读取文件数据"""
@@ -20,11 +49,13 @@ def read_file(filename):
         for line in f:
             try:
                 label, content = line.strip().split('\t')
-                contents.append(list(content))
-                labels.append(label)
+                if content:
+                    contents.append(list(native_content(content)))
+                    labels.append(native_content(label))
             except:
                 pass
     return contents, labels
+
 
 def build_vocab(train_dir, vocab_dir, vocab_size=5000):
     """根据训练集构建词汇表，存储"""
@@ -33,33 +64,37 @@ def build_vocab(train_dir, vocab_dir, vocab_size=5000):
     all_data = []
     for content in data_train:
         all_data.extend(content)
-
     counter = Counter(all_data)
     count_pairs = counter.most_common(vocab_size - 1)
     words, _ = list(zip(*count_pairs))
     # 添加一个 <PAD> 来将所有文本pad为同一长度
     words = ['<PAD>'] + list(words)
-
     open_file(vocab_dir, mode='w').write('\n'.join(words) + '\n')
+
 
 def read_vocab(vocab_dir):
     """读取词汇表"""
-    words = open_file(vocab_dir).read().strip().split('\n')
+    # words = open_file(vocab_dir).read().strip().split('\n')
+    with open_file(vocab_dir) as fp:
+        # 如果是py2 则每个值都转化为unicode
+        words = [native_content(_.strip()) for _ in fp.readlines()]
     word_to_id = dict(zip(words, range(len(words))))
-
     return words, word_to_id
+
 
 def read_category():
     """读取分类目录，固定"""
     categories = ['体育', '财经', '房产', '家居',
-        '教育', '科技', '时尚', '时政', '游戏', '娱乐']
+                  '教育', '科技', '时尚', '时政', '游戏', '娱乐']
     cat_to_id = dict(zip(categories, range(len(categories))))
 
     return categories, cat_to_id
 
+
 def to_words(content, words):
     """将id表示的内容转换为文字"""
     return ''.join(words[x] for x in content)
+
 
 def process_file(filename, word_to_id, cat_to_id, max_length=600):
     """将文件转换为id表示"""
@@ -67,7 +102,8 @@ def process_file(filename, word_to_id, cat_to_id, max_length=600):
 
     data_id, label_id = [], []
     for i in range(len(contents)):
-        data_id.append([word_to_id[x] for x in contents[i] if x in word_to_id])
+        data_id.append(
+            [word_to_id[x] for x in contents[i] if x in word_to_id])
         label_id.append(cat_to_id[labels[i]])
 
     # 使用keras提供的pad_sequences来将文本pad为固定长度
@@ -75,6 +111,7 @@ def process_file(filename, word_to_id, cat_to_id, max_length=600):
     y_pad = kr.utils.to_categorical(label_id)  # 将标签转换为one-hot表示
 
     return x_pad, y_pad
+
 
 def batch_iter(x, y, batch_size=64):
     """生成批次数据"""
--- predict.py
+++ predict.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from cnn_model import *
@@ -8,36 +8,47 @@ import tensorflow.contrib.keras as kr
 import time
 from datetime import timedelta
 
+try:
+    bool(type(unicode))
+except NameError:
+    unicode = str
+
 base_dir = 'data/cnews'
 vocab_dir = os.path.join(base_dir, 'cnews.vocab.txt')
 
 save_dir = 'checkpoints/textcnn'
-save_path = os.path.join(save_dir, 'best_validation')   # 最佳验证结果保存路径
+save_path = os.path.join(save_dir, 'best_validation')  # 最佳验证结果保存路径
 
-def predict():
-    config = TCNNConfig()
-    categories, cat_to_id = read_category()
-    words, word_to_id = read_vocab(vocab_dir)
-    config.vocab_size = len(words)
-    model = TextCNN(config)
 
-    session = tf.Session()
-    session.run(tf.global_variables_initializer())
-    saver = tf.train.Saver()
-    saver.restore(sess=session, save_path=save_path)  # 读取保存的模型
+class CnnModel:
+    def __init__(self):
+        self.config = TCNNConfig()
+        self.categories, self.cat_to_id = read_category()
+        self.words, self.word_to_id = read_vocab(vocab_dir)
+        self.config.vocab_size = len(self.words)
+        self.model = TextCNN(self.config)
 
-    content = "三星ST550以全新的拍摄方式超越了以往任何一款数码相机"
-    data = [word_to_id[x] for x in content if x in word_to_id]
+        self.session = tf.Session()
+        self.session.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()
+        saver.restore(sess=self.session, save_path=save_path)  # 读取保存的模型
 
-    data = kr.preprocessing.sequence.pad_sequences([data], config.seq_length)
-    feed_dict = {
-        model.input_x: data,
-        model.keep_prob: 1.0
-    }
+    def predict(self, message):
+        # 支持不论在python2还是python3下训练的模型都可以在2后者3的环境下运行
+        content = unicode(message)
+        data = [self.word_to_id[x] for x in content if x in self.word_to_id]
 
-    y_pred_cls = session.run(model.y_pred_cls, feed_dict=feed_dict)
-    print(categories[y_pred_cls[0]])
+        feed_dict = {
+            self.model.input_x: kr.preprocessing.sequence.pad_sequences([data], self.config.seq_length),
+            self.model.keep_prob: 1.0
+        }
+
+        y_pred_cls = self.session.run(self.model.y_pred_cls, feed_dict=feed_dict)
+        return self.categories[y_pred_cls[0]]
 
 
 if __name__ == '__main__':
-    predict()
+    cnn_model = CnnModel()
+    test_demo = ['三星ST550以全新的拍摄方式超越了以往任何一款数码相机', '热火vs骑士前瞻：皇帝回乡二番战 东部次席唾手可得新浪体育讯北京时间3月30日7:00']
+    for i in test_demo:
+        print(cnn_model.predict(i))
